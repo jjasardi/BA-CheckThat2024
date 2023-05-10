4189388bb1069a379c20ab845d40133977540a06
vode <vode@zhaw.ch>
2023-05-10 13:39:19 +0200

    gotta do async magic

--- checkthat2023/ask_gpt.py
+++ checkthat2023/ask_gpt.py
@@ -1,4 +1,5 @@
 
+import asyncio
 from pathlib import Path
 
 from checkthat2023.tasks.task1a import load
@@ -12,6 +13,11 @@ task1a = load(data_path, dev=False)
 
 clf = GPTClf(secret_file=secret_file, cache_file=cache_file)
 
-train_preds = await clf.predict(task1a.train)
-dev_preds = await clf.predict(task1a.dev)
-test_preds = await clf.predict(task1a.test)
+
+async def dew_it():
+    _ = await clf.predict(task1a.train)
+    _ = await clf.predict(task1a.dev)
+    _ = await clf.predict(task1a.test)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(dew_it())
