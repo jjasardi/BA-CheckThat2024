9c3002c6e11ea8e7e1553c249023a203b48d00e7
vode <vode@zhaw.ch>
2023-05-11 13:59:23 +0200

    try hand-hacked hyperparam opt

--- checkthat2023/scrap.py
+++ checkthat2023/scrap.py
@@ -12,19 +12,24 @@ from sklearn.metrics import (
     precision_recall_curve,
     f1_score,
 )
+from sklearn.model_selection import ParameterSampler
+from sklearn.utils.fixes import loguniform
 
 import numpy as np
 
 import torch
 
+from scipy import stats
+
 from checkthat2023.tasks.task1a import load
-from checkthat2023.kernel_stuff.kernel import KernelData, KernelList
+from checkthat2023.kernel_stuff.kernel import KernelData, KernelList, DistData
 
 
 matplotlib.use('QtAgg')
 
 data_path = Path('data')
 kernel_folder = Path('kernel_data/')
+dist_folder = Path("dist_data/")
 
 task1a = load(data_folder=data_path, dev=False)
 
@@ -43,29 +48,43 @@ kernels = [
     KernelData.load_from(kf)
     for kf in kernel_folder.glob("*")
 ]
+dist = DistData.load_from(dist_folder / "token-alignment")
 
-kl = KernelList(kernels=kernels, name="avg-kernel")
+kl = KernelList(kernels=kernels + [dist.rbf(gamma=10.)], name="avg-kernel")
 
-for c in np.logspace(-3, 3, base=10, num=7):
+
+param_space = list(ParameterSampler(
+    param_distributions={
+        "svm_c": loguniform(1e-3, 1e3),
+        "gamma": loguniform(1e-4, 1e2),
+        "kernel_weights": stats.dirichlet(np.ones(len(kernels) + 1) * 100),
+    },
+    n_iter=100,
+    random_state=0xdeadbeef,
+))
+
+
+def experiment(
+    svm_c: float,
+    gamma: float,
+    kernel_weights: np.array,
+):
+    kernel_list = KernelList(kernels + [dist.rbf(gamma)], name="")
     svm = SVC(
-        C=c,
+        C=svm_c,
         kernel='precomputed',
         class_weight='balanced',
         random_state=0xdeadbeef,
     )
-    svm.fit(kl.train(), y_train)
-    scores = svm.decision_function(kl.dev())
-    ps, rs, ths = precision_recall_curve(
+    w = torch.tensor(kernel_weights[0])
+    k_train = kernel_list.train(w)
+    k_dev = kernel_list.dev(w)
+
+    svm.fit(k_train, y_train)
+    f1 = f1_score(
         y_true=y_dev,
-        probas_pred=scores,
-        pos_label=1,
-    )
-    f1 = (2. * ps * rs) / (ps + rs)
-    ix = f1.argmax()
-    base_f1 = f1_score(
-        y_true=y_dev,
-        y_pred=svm.predict(kl.dev()),
+        y_pred=svm.predict(k_dev),
         pos_label=1,
         average='binary',
     )
-    print(f"C: {c:.3f}\tbest_thresh: {ths[ix]:.3f}\tF1: {f1[ix]:.3f}\tF1-base: {base_f1:.3f}")
+    return f1
