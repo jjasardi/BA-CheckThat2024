2980b94c1f4c07de4e2b41285b017e490722c4fc
vode <vode@zhaw.ch>
2023-05-11 02:00:40 +0200

    play with average kernel

--- checkthat2023/scrap.py
+++ checkthat2023/scrap.py
@@ -5,25 +5,67 @@ from pathlib import Path
 import matplotlib
 from matplotlib import pyplot as plt
 
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.metrics import (
+    RocCurveDisplay,
+    PrecisionRecallDisplay,
+    precision_recall_curve,
+    f1_score,
+)
 
-from checkthat2023.preprocessing.text import cardiffnlp_preprocess
+import numpy as np
+
+import torch
+
 from checkthat2023.tasks.task1a import load
+from checkthat2023.kernel_stuff.kernel import KernelData, KernelList
 
 
 matplotlib.use('QtAgg')
 
 data_path = Path('data')
-gpt_cache = Path('cache/gpt-cache.json')
-electra_kernel = Path('kernel_data/electra')
+kernel_folder = Path('kernel_data/')
 
 task1a = load(data_folder=data_path, dev=False)
 
-with gpt_cache.open('r') as fin:
-    gpt_data = json.load(fin)
+y_train = [
+    s.class_label
+    for s in task1a.train
+]
+y_dev = [
+    s.class_label
+    for s in task1a.dev
+]
 
-from checkthat2023.img_kernel_untrained import Embedder
+fig, ax = plt.subplots()
 
-e = Embedder()
+kernels = [
+    KernelData.load_from(kf)
+    for kf in kernel_folder.glob("*")
+]
 
-es = e.embeddings(task1a.train[:2])
+kl = KernelList(kernels=kernels, name="avg-kernel")
+
+for c in np.logspace(-3, 3, base=10, num=7):
+    svm = SVC(
+        C=c,
+        kernel='precomputed',
+        class_weight='balanced',
+        random_state=0xdeadbeef,
+    )
+    svm.fit(kl.train(), y_train)
+    scores = svm.decision_function(kl.dev())
+    ps, rs, ths = precision_recall_curve(
+        y_true=y_dev,
+        probas_pred=scores,
+        pos_label=1,
+    )
+    f1 = (2. * ps * rs) / (ps + rs)
+    ix = f1.argmax()
+    base_f1 = f1_score(
+        y_true=y_dev,
+        y_pred=svm.predict(kl.dev()),
+        pos_label=1,
+        average='binary',
+    )
+    print(f"C: {c:.3f}\tbest_thresh: {ths[ix]:.3f}\tF1: {f1[ix]:.3f}\tF1-base: {base_f1:.3f}")
