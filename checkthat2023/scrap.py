962cc7b646fb3799aeb816fb3a9e513aa77d1fc9
vode <vode@zhaw.ch>
2023-06-03 16:30:02 +0200

    wip kernel mixing strats

--- checkthat2023/scrap.py
+++ checkthat2023/scrap.py
@@ -11,6 +11,8 @@ from sklearn.metrics import (
     PrecisionRecallDisplay,
     precision_recall_curve,
     f1_score,
+    hinge_loss,
+    log_loss,
 )
 from sklearn.model_selection import ParameterSampler
 from sklearn.utils.fixes import loguniform
@@ -20,6 +22,8 @@ import numpy as np
 import torch
 
 from scipy import stats
+from scipy.optimize import Bounds, LinearConstraint, minimize
+from scipy.special import softmax
 
 from checkthat2023.tasks.task1a import load
 from checkthat2023.kernel_stuff.kernel import KernelData, KernelList, DistData
@@ -53,17 +57,6 @@ dist = DistData.load_from(dist_folder / "token-alignment")
 kl = KernelList(kernels=kernels + [dist.rbf(gamma=10.)], name="avg-kernel")
 
 
-param_space = list(ParameterSampler(
-    param_distributions={
-        "svm_c": loguniform(1e-3, 1e3),
-        "gamma": loguniform(1e-4, 1e2),
-        "kernel_weights": stats.dirichlet(np.ones(len(kernels) + 1) * 100),
-    },
-    n_iter=100,
-    random_state=0xdeadbeef,
-))
-
-
 def experiment(
     svm_c: float,
     gamma: float,
@@ -75,16 +68,37 @@ def experiment(
         kernel='precomputed',
         class_weight='balanced',
         random_state=0xdeadbeef,
+        probability=True,
     )
-    w = torch.tensor(kernel_weights[0])
+    w = torch.tensor(kernel_weights)
     k_train = kernel_list.train(w)
     k_dev = kernel_list.dev(w)
 
     svm.fit(k_train, y_train)
-    f1 = f1_score(
-        y_true=y_dev,
-        y_pred=svm.predict(k_dev),
-        pos_label=1,
-        average='binary',
+    # f1 = f1_score(
+    #     y_true=y_dev,
+    #     y_pred=svm.predict(k_dev),
+    #     pos_label=1,
+    #     average='binary',
+    # )
+    # return -f1
+    # return hinge_loss(y_true=y_dev, pred_decision=svm.decision_function(k_dev))
+    return log_loss(y_true=y_dev, y_pred=svm.predict_proba(k_dev))
+
+
+def wrapper(x: np.array):
+    return experiment(
+        svm_c=np.exp(x[0]),
+        gamma=np.exp(x[1]),
+        kernel_weights=softmax(x[2:]),
     )
-    return f1
+
+x0 = np.zeros(len(kernels) + 1 + 2)
+
+opt_res = minimize(
+    fun=wrapper,
+    x0=x0,
+    method="Nelder-Mead",
+    options={'verbose': 3}
+)
+
