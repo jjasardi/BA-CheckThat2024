c6269ced3c2a06dfce638ad2fc0a8eecdb386aa4
vode <vode@zhaw.ch>
2023-06-05 16:12:02 +0200

    plots and results for checkthat2023 paper

--- checkthat2023/paper/experiments.py
+++ checkthat2023/paper/experiments.py
@@ -1,76 +1,168 @@
 
 from pathlib import Path
+import json
 
 import numpy as np
 from sklearn.svm import SVC
 
-from matplotlib import pyplot as plt
-
-from sklearn.metrics import RocCurveDisplay, PrecisionRecallDisplay
-
 from checkthat2023.tasks.task1a import load
 from checkthat2023.kernel_stuff.kernel import KernelList, KernelData
-
-task1a = load(Path("./data"), dev=False)
-
-y_train = [s.class_label for s in task1a.train]
-y_dev = [s.class_label for s in task1a.dev]
-y_test = [s.class_label for s in task1a.test]
-
-
-base_kernels = {
-    folder.name: KernelData.load_from(folder)
-    for folder in Path("./kernel_data/").glob("*")
-}
-
-submission_kernel = KernelList(name="submission", kernels=[
-    base_kernels[name]
-    for name in ["electra", "gpt-ngram", "multimodal", "ngram"]
-]).kernel_data()
-
-kernels = {
-    submission_kernel.name: submission_kernel,
-    "all": KernelList(name="all", kernels=list(base_kernels.values())).kernel_data(),
-    **base_kernels,
-}
+from checkthat2023.paper.plots import (
+    ClfCollection,
+    ClfInfo,
+    roc_plots,
+    pr_curves,
+    optimize_threshold,
+    print_fscores,
+)
 
 
-fig, ax = plt.subplots()
-ax.set_xlim(.0, 1.01)
-ax.set_ylim(.0, 1.01)
-fig.set_size_inches(10, 10)
-fig.set_tight_layout(True)
+def kernel_classifiers(
+    y_train: np.array,
+    y_dev: np.array,
+):
+    base_kernels = {
+        'img-untrained-kernel': KernelData.load_from(Path("kernel_data/img-untrained")),
+        'text-ngram': KernelData.load_from(Path("kernel_data/ngram/")),
+        'gpt-ngram': KernelData.load_from(Path("kernel_data/gpt-ngram")),
+        'electra-kernel': KernelData.load_from(Path("kernel_data/electra")),
+        'multi-modal-kernel': KernelData.load_from(Path("kernel_data/multimodal")),
+    }
 
-ax.plot([0., 1.], [0., 1.], linestyle='--', label="random", c='b')
+    kernels = {
+        "submission": KernelList(name="submission", kernels=[
+            base_kernels[s]
+            for s in ["text-ngram", "gpt-ngram", "electra-kernel", "multi-modal-kernel"]
+        ]).kernel_data(),
+        "all-kernels": KernelList(
+            name="all-kernel", kernels=list(base_kernels.values())).kernel_data(),
+        **base_kernels,
+    }
 
-for name, kernel in kernels.items():
-    svm = SVC(C=1., kernel='precomputed', class_weight="balanced", random_state=0xdeadbeef)
-    svm.fit(kernel.train, y_train)
+    systems = [
+        "img-untrained-kernel",
+        "text-ngram",
+        "gpt-ngram",
+        "electra-kernel",
+        "multi-modal-kernel",
+        "submission",
+        "all-kernels",
+    ]
+    clf_infos = {}
 
-    _ = RocCurveDisplay.from_predictions(
-        y_true=y_test,
-        y_pred=svm.decision_function(kernel.test),
-        name=name,
-        ax=ax,
+    for s in systems:
+        k = kernels[s]
+        svm = SVC(
+            C=1., kernel='precomputed', class_weight='balanced', random_state=0xdeadbeef)
+        svm.fit(k.train, y_train)
+
+        test_scores = svm.decision_function(k.test)
+        base_preds = svm.predict(k.test)
+
+        dev_scores = svm.decision_function(k.dev)
+        th = optimize_threshold(y_dev, dev_scores)
+
+        optimized_preds = test_scores >= th
+
+        info = ClfInfo(
+            name=s,
+            test_scores=test_scores,
+            test_preds_base=base_preds,
+            test_preds_tuned=optimized_preds,
+        )
+
+        clf_infos[s] = info
+
+    return ClfCollection(
+        systems=systems,
+        infos=clf_infos,
     )
 
 
-electra_logits = np.load("./preds/text_model_test_logits.npy")
-_ = RocCurveDisplay.from_predictions(
-    y_true=y_test,
-    y_pred=electra_logits[:, 1],
-    name="electra-logits",
-    ax=ax,
-)
+def direct_classifiers():
+    systems = [
+        "gpt-answer",
+        "electra-clf",
+        "multi-modal-clf",
+    ]
+    clf_infos = {}
 
-multimodal_logits = np.load('./preds/multimodal_test_logits.npy')
-_ = RocCurveDisplay.from_predictions(
-    y_true=y_test,
-    y_pred=multimodal_logits[:, 1],
-    name="multi-modal-logits",
-    ax=ax,
-)
+    electra_scores = np.load("preds/text_model_test_logits.npy")
+    clf_infos['electra-clf'] = ClfInfo(
+        name="electra-clf",
+        test_scores=electra_scores[:, 1],
+        test_preds_base=electra_scores.argmax(axis=1),
+        test_preds_tuned=None,
+    )
 
-ax.legend()
+    multi_scores = np.load("preds/multimodal_test_logits.npy")
+    clf_infos['multi-modal-clf'] = ClfInfo(
+        name="multi-modal-clf",
+        test_scores=multi_scores[:, 1],
+        test_preds_base=multi_scores.argmax(axis=1),
+        test_preds_tuned=None,
+    )
 
-plt.show()
+    with open("preds/gpt-predictions.json", 'r') as fin:
+        answers = json.load(fin)
+    answers = np.array([
+        d['class_label'] == 'Yes'
+        for d in answers
+    ]).astype(int)
+
+    clf_infos["gpt-answer"] = ClfInfo(
+        name="gpt-answer",
+        test_scores=None,
+        test_preds_tuned=None,
+        test_preds_base=answers,
+    )
+
+    return ClfCollection(
+        systems=systems,
+        infos=clf_infos,
+    )
+
+
+def main():
+    task1a = load(Path("./data"), dev=False)
+
+    y_train = np.array([s.class_label for s in task1a.train]).astype(int)
+    y_dev = np.array([s.class_label for s in task1a.dev]).astype(int)
+    y_test = np.array([s.class_label for s in task1a.test]).astype(int)
+
+    kernel_collection = kernel_classifiers(y_train=y_train, y_dev=y_dev)
+    clf_collection = direct_classifiers()
+
+    # roc_plots(collection=kernel_collection, y_test=y_test)
+    # pr_curves(collection=kernel_collection, y_test=y_test)
+
+    # roc_plots(collection=clf_collection, y_test=y_test)
+    # pr_curves(collection=clf_collection, y_test=y_test)
+
+    all_systems = [
+        "gpt-answer",
+        "img-untrained-kernel",
+        "text-ngram",
+        "gpt-ngram",
+        "electra-kernel",
+        "multi-modal-kernel",
+        "submission",
+        "all-kernels",
+        "electra-clf",
+        "multi-modal-clf",
+    ]
+    all_infos = {
+        **kernel_collection.infos,
+        **clf_collection.infos
+    }
+    full_collection = ClfCollection(systems=all_systems, infos=all_infos)
+
+    roc_plots(full_collection, y_test=y_test, save_path="paper/all_roc.png")
+    pr_curves(full_collection, y_test=y_test, save_path="paper/all_pr.png")
+
+    print_fscores(full_collection, y_test=y_test)
+
+
+if __name__ == "__main__":
+    main()
+
--- checkthat2023/paper/plots.py
+++ checkthat2023/paper/plots.py
@@ -1,16 +1,276 @@
 
-from typing import List, Dict
+from dataclasses import dataclass
+from typing import List, Dict, Optional
 
-from sklearn.metrics import RocCurveDisplay, PrecisionRecallDisplay
+from sklearn.metrics import (
+    RocCurveDisplay,
+    PrecisionRecallDisplay,
+    precision_recall_fscore_support,
+    precision_recall_curve,
+)
 
 from matplotlib import pyplot as plt
 
 import numpy as np
 
 
+@dataclass
+class ClfInfo:
+    name: str
+    test_preds_base: np.array
+    test_scores: Optional[np.array] = None
+    test_preds_tuned: Optional[np.array] = None
+
+
+@dataclass
+class ClfCollection:
+    systems: List[str]
+    infos: Dict[str, ClfInfo]
+
+
+@dataclass
+class RocPoint:
+    tpr: float
+    fpr: float
+
+
+@dataclass
+class PRPoint:
+    precision: float
+    recall: float
+    f1: float
+
+
+def roc_point(y_true: np.array, y_pred: np.array):
+    true = y_true.astype(int)
+    pred = y_pred.astype(int)
+
+    tpr = (pred[true == 1] == 1).mean()
+    fpr = (pred[true == 0] == 1).mean()
+    return RocPoint(
+        tpr=tpr,
+        fpr=fpr,
+    )
+
+
+def pr_point(y_true: np.array, y_pred: np.array):
+    p, r, f1, _ = precision_recall_fscore_support(
+        y_true=y_true,
+        y_pred=y_pred,
+        pos_label=1,
+        average='binary',
+    )
+    return PRPoint(
+        precision=p,
+        recall=r,
+        f1=f1,
+    )
+
+
+def optimize_threshold(y_true, y_scores):
+    ps, rs, ths = precision_recall_curve(
+        y_true=y_true,
+        probas_pred=y_scores,
+        pos_label=1,
+    )
+
+    f1s = (2 * ps * rs) / (ps + rs)
+    f1s = f1s[:-1]
+
+    not_nan = np.isfinite(f1s)
+
+    return ths[not_nan][f1s[not_nan].argmax()]
+
+
 def roc_plots(
-    systems: List[str],
-    test_scores: Dict[str, np.array],
-    dev_scores: Dict[str, np.array],
+    collection: ClfCollection,
+    y_test: np.array,
+    save_path: Optional[str] = None,
 ):
-    pass
+    fig, ax = plt.subplots()
+    ax.set_xlim(.0, 1.01)
+    ax.set_ylim(.0, 1.01)
+    fig.set_size_inches(10, 10)
+    fig.set_tight_layout(True)
+
+    ax.plot([0., 1.], [0., 1.], linestyle='--', label="random", c='b')
+
+    base_rocs = []
+    tuned_rocs = []
+
+    # plot curves and points
+    for s in collection.systems:
+        info = collection.infos[s]
+        if info.test_scores is not None:
+            _ = RocCurveDisplay.from_predictions(
+                y_true=y_test,
+                y_pred=info.test_scores,
+                name=s,
+                ax=ax,
+            )
+            base_rocs.append(
+                roc_point(y_true=y_test, y_pred=info.test_preds_base))
+            if info.test_preds_tuned is not None:
+                tuned_rocs.append(
+                    roc_point(y_true=y_test, y_pred=info.test_preds_tuned))
+        else:
+            roc = roc_point(y_true=y_test, y_pred=info.test_preds_base)
+            ax.scatter(
+                x=[roc.fpr],
+                y=[roc.tpr],
+                marker='D',
+                label=s,
+                s=52,
+            )
+
+    # plot default operating points
+    if len(base_rocs) > 0:
+        ax.scatter(
+            x=[r.fpr for r in base_rocs],
+            y=[r.tpr for r in base_rocs],
+            marker='x',
+            c='k',
+            label="Base Operating Point",
+            s=52,
+        )
+
+    # plot tuned operating points
+    if len(tuned_rocs) > 0:
+        ax.scatter(
+            x=[r.fpr for r in tuned_rocs],
+            y=[r.tpr for r in tuned_rocs],
+            marker='x',
+            c='r',
+            label="F1 Tuned Operating Point",
+            s=52,
+        )
+
+    ax.legend()
+
+    if save_path is not None:
+        plt.savefig(save_path)
+    else:
+        plt.show()
+
+
+def pr_curves(
+    collection: ClfCollection,
+    y_test: np.array,
+    save_path: Optional[str] = None,
+):
+    fig, ax = plt.subplots()
+    ax.set_xlim(.0, 1.01)
+    ax.set_ylim(.0, 1.01)
+    fig.set_size_inches(10, 10)
+    fig.set_tight_layout(True)
+
+    # plot lines of constant F1
+    for fixed_f1 in np.linspace(.1, .9, num=9):
+        ps = np.linspace(.01, .99, num=98)
+        rs = (- fixed_f1 * ps) / (fixed_f1 - 2 * ps)
+        ax.plot(rs[rs > 0], ps[rs > 0], c='k', alpha=.3)
+        txt_x = .95
+        txt_y = (- fixed_f1 * 0.95) / (fixed_f1 - 2 * 0.95) + .01
+        ax.text(x=txt_x, y=txt_y, s=f"F1 {fixed_f1:.1f}")
+
+    base_pr = []
+    tuned_pr = []
+
+    # plot curves and points
+    for s in collection.systems:
+        info = collection.infos[s]
+        if info.test_scores is not None:
+            _ = PrecisionRecallDisplay.from_predictions(
+                y_true=y_test,
+                y_pred=info.test_scores,
+                name=s,
+                ax=ax,
+            )
+            base_pr.append(
+                pr_point(y_true=y_test, y_pred=info.test_preds_base))
+            if info.test_preds_tuned is not None:
+                tuned_pr.append(
+                    pr_point(y_true=y_test, y_pred=info.test_preds_tuned))
+        else:
+            pr = pr_point(y_true=y_test, y_pred=info.test_preds_base)
+            ax.scatter(
+                x=[pr.recall],
+                y=[pr.precision],
+                marker='D',
+                label=s,
+                s=52,
+            )
+
+    # plot default operating points
+    if len(base_pr) > 0:
+        ax.scatter(
+            x=[p.recall for p in base_pr],
+            y=[p.precision for p in base_pr],
+            marker='x',
+            c='k',
+            label="Base Operating Point",
+            s=52,
+        )
+
+    # plot tuned operating points
+    if len(tuned_pr) > 0:
+        ax.scatter(
+            x=[p.recall for p in tuned_pr],
+            y=[p.precision for p in tuned_pr],
+            marker='x',
+            c='r',
+            label="F1 Tuned Operating Point",
+            s=52,
+        )
+
+    ax.legend()
+
+    if save_path is not None:
+        plt.savefig(save_path)
+    else:
+        plt.show()
+
+
+def print_fscores(
+    collection: ClfCollection,
+    y_test: np.array,
+):
+    for s in collection.systems:
+        info = collection.infos[s]
+
+        default = pr_point(
+            y_true=y_test,
+            y_pred=info.test_preds_base,
+        )
+        if info.test_preds_tuned is not None:
+            tuned = pr_point(
+                y_true=y_test,
+                y_pred=info.test_preds_tuned,
+            )
+        else:
+            tuned = None
+        if info.test_scores is not None:
+            th = optimize_threshold(
+                y_true=y_test,
+                y_scores=info.test_scores,
+            )
+            opt_preds = (info.test_scores >= th).astype(int)
+            opt = pr_point(
+                y_true=y_test,
+                y_pred=opt_preds,
+            )
+        else:
+            opt = None
+
+        line = f"{s} & {default.precision:.3f} & {default.recall:.3f} & {default.f1:.3f}"
+        if tuned is not None:
+            line += f" & {tuned.precision:.3f} & {tuned.recall:.3f} & {tuned.f1:.3f}"
+        else:
+            line += f" & - & - & -"
+        if opt is not None:
+            line += f" & {opt.precision:.3f} & {opt.recall:.3f} & {opt.f1:.3f}"
+        else:
+            line += f" & - & - & -"
+        line += " \\\\"
+
+        print(line)
