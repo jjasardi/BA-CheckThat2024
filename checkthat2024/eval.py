5bf0fbd105a31ad24e0b1d67a0c4d1ddfa5a097c
jasarard <jasarard@students.zhaw.ch>
2024-03-31 17:37:30 +0200

    function to see misclassified samples

--- checkthat2024/eval.py
+++ checkthat2024/eval.py
@@ -6,6 +6,8 @@ from sklearn.metrics import (
 )
 
 import numpy as np
+import torch
+from torch.nn import CrossEntropyLoss
 
 
 def hf_eval(eval_pred):
@@ -17,19 +19,19 @@ def hf_eval(eval_pred):
         y_true=y_true,
         y_pred=y_pred,
         pos_label=1,
-        average='binary',
+        average="binary",
     )
     recall = recall_score(
         y_true=y_true,
         y_pred=y_pred,
         pos_label=1,
-        average='binary',
+        average="binary",
     )
     f1 = f1_score(
         y_true=y_true,
         y_pred=y_pred,
         pos_label=1,
-        average='binary',
+        average="binary",
     )
 
     return {
@@ -40,3 +42,49 @@ def hf_eval(eval_pred):
     }
 
 
+def get_misclassified_samples(x_test, y_test, logits):
+    predicted_labels = np.argmax(logits, axis=1)
+    losses = compute_losses(logits, y_test)
+
+    misclassified_indices = np.nonzero(predicted_labels != y_test)[0]
+    misclassified_samples = [
+        (x_test[index], predicted_labels[index], y_test[index], losses[index].item())
+        for index in misclassified_indices
+    ]
+    misclassified_samples.sort(key=lambda x: x[3], reverse=True)
+    return misclassified_samples
+
+
+def compute_losses(logits, y_test):
+    loss_func = CrossEntropyLoss(reduction="none")
+
+    logits_tensor = torch.tensor(logits)
+    y_test_tensor = torch.tensor(y_test, dtype=torch.long)
+
+    losses = loss_func(logits_tensor, y_test_tensor)
+    return losses
+
+
+if __name__ == "__main__":
+    from checkthat2024.task1a import load
+    from argparse import ArgumentParser
+    from pathlib import Path
+
+    parser = ArgumentParser()
+    parser.add_argument("-d", "--data", dest="data_folder", type=Path, required=True)
+    parser.add_argument("--dev", dest="dev_mode", action="store_true")
+    parser.add_argument("-l", "--logits", dest="logits_file", type=str, required=True)
+    args = parser.parse_args()
+
+    logits = np.load(args.logits_file)
+    print(logits)
+    dataset = load(data_folder=args.data_folder, dev=args.dev_mode)
+    x_test = [s.text for s in dataset.test]
+    y_test = [s.class_label for s in dataset.test]
+
+    misclassified_samples = get_misclassified_samples(x_test, y_test, logits)
+
+    for text, predicted_label, ground_truth_label, loss in misclassified_samples:
+        print(
+            f"{text}, Predicted Label = {predicted_label}, Ground Truth Label = {ground_truth_label}, loss = {loss}"
+        )
