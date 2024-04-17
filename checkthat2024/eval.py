c828b02c2f7616163e03598545f102799312b98a
jasarard <jasarard@students.zhaw.ch>
2024-04-17 11:28:17 +0200

    added precision recall plot for multiple logits files

--- checkthat2024/eval.py
+++ checkthat2024/eval.py
@@ -8,6 +8,8 @@ from sklearn.metrics import (
 import numpy as np
 import torch
 from torch.nn import CrossEntropyLoss
+from sklearn.metrics import PrecisionRecallDisplay
+import wandb
 
 
 def hf_eval(eval_pred):
@@ -42,17 +44,47 @@ def hf_eval(eval_pred):
     }
 
 
-def get_misclassified_samples(x_test, y_test, logits):
-    predicted_labels = np.argmax(logits, axis=1)
-    losses = compute_losses(logits, y_test)
+def misclassified_samples(x_test, y_test, logits, model_names):
+    for logits, model_name in zip(logits, model_names):
+        predicted_labels = np.argmax(logits, axis=1)
+        losses = compute_losses(logits, y_test)
 
-    misclassified_indices = np.nonzero(predicted_labels != y_test)[0]
-    misclassified_samples = [
-        (x_test[index], predicted_labels[index], y_test[index], losses[index].item())
-        for index in misclassified_indices
-    ]
-    misclassified_samples.sort(key=lambda x: x[3], reverse=True)
-    return misclassified_samples
+        misclassified_indices = np.nonzero(predicted_labels != y_test)[0]
+        misclassified_samples = [
+            (x_test[index], predicted_labels[index], y_test[index], losses[index].item())
+            for index in misclassified_indices
+        ]
+        misclassified_samples.sort(key=lambda x: x[3], reverse=True)
+
+        print(model_name)
+        for text, predicted_label, ground_truth_label, loss in misclassified_samples:
+            print(
+                f"{text}, Predicted Label = {predicted_label}, Ground Truth Label = {ground_truth_label}, loss = {loss}"
+            )
+
+
+def compute_precision_recall(y_test, logits):
+    y_test = np.array(y_test, dtype=int)
+    logits_tensor = torch.tensor(logits)
+    probs = torch.nn.Softmax(dim=1)(logits_tensor)[:, 1]
+    display = PrecisionRecallDisplay.from_predictions(y_test, probs)
+    return display
+
+
+def precision_recall_plot(y_test, logits, model_names):
+    wandb.init(project="ba24-check-worthiness-estimation", group="general-plots", name="precision_recall_plot")
+
+    combined_display = PrecisionRecallDisplay()
+
+    for logits, model_name in zip(logits, model_names):
+        display = compute_precision_recall(y_test, logits)
+        display.name = model_name
+        combined_display.append_display(display)
+
+    _ = combined_display.plot(ax=display.ax_)
+    _ = display.ax_.set_title("Precision-Recall curves")
+
+    wandb.log({"Precision-Recall Curve)": wandb.Image(display.figure_)})
 
 
 def compute_losses(logits, y_test):
@@ -73,17 +105,17 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-d", "--data", dest="data_folder", type=Path, required=True)
     parser.add_argument("--dev", dest="dev_mode", action="store_true")
-    parser.add_argument("-l", "--logits", dest="logits_file", type=str, required=True)
+    parser.add_argument("-l", "--logits", dest="logits_files", nargs="+", type=str, required=True)
+    parser.add_argument("-n", "--model-names", dest="model_names", nargs="+", required=True)
     args = parser.parse_args()
 
-    logits = np.load(args.logits_file)
+    logits = []
+    for logits_file in args.logits_files:
+        logits.append(np.load("./model_dump/CT_24/" + logits_file))
     dataset = load(data_folder=args.data_folder, dev=args.dev_mode)
     x_test = [s.text for s in dataset.test]
     y_test = [s.class_label for s in dataset.test]
 
-    misclassified_samples = get_misclassified_samples(x_test, y_test, logits)
+    misclassified_samples = misclassified_samples(x_test, y_test, logits, args.model_names)
 
-    for text, predicted_label, ground_truth_label, loss in misclassified_samples:
-        print(
-            f"{text}, Predicted Label = {predicted_label}, Ground Truth Label = {ground_truth_label}, loss = {loss}"
-        )
+    precision_recall_plot(y_test, logits, args.model_names)
